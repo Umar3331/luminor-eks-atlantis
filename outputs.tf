b60b5aafedc21985e337315bd1bea1ccc87a1caf
Ubuntu <ubuntu@ip-172-31-38-204.eu-north-1.compute.internal>
2024-11-05 14:52:54 +0000

    2nd test

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "cluster_endpoint" {
   description = "EKS cluster endpoint"
   value       = module.eks.cluster_endpoint
 }
-#testing PR for Atlantis
+
 output "cluster_certificate_authority_data" {
   description = "EKS cluster CA data"
   value       = module.eks.cluster_certificate_authority_data

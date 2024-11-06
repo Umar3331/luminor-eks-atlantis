3b66b0ed1a71510e1408c701b96a8bfbb1ca736a
Ubuntu <ubuntu@ip-172-31-38-204.eu-north-1.compute.internal>
2024-11-06 14:38:30 +0000

    test atlantis

--- main.tf
+++ main.tf
@@ -4,13 +4,15 @@ module "vpc" {
 
   name                 = "eks-vpc"
   cidr                 = "10.0.0.0/16"
-  azs                  = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs                  = ["eu-north-1a", "eu-north-1b"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
+
+  #depends_on = [module.eks]
 
   tags = {
     "Name" = "eks-vpc"
